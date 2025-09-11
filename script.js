eba0ab400eb39ee32e177c0490a263d63a2d8895
Owen <owen.franckh@gmail.com>
2025-09-11 15:10:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const nav = document.querySelector('.nav-links');
 burger.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+
+const categories = document.querySelectorAll('.category h2');
+
+categories.forEach(cat => {
+    cat.addEventListener('click', () => {
+        const menu = cat.nextElementSibling;
+
+        // Si déjà ouvert, on ferme
+        if(menu.style.display === 'grid') {
+            menu.style.display = 'none';
+        } else {
+            // On ferme tous les autres menus
+            document.querySelectorAll('.category .menu-items').forEach(m => m.style.display = 'none');
+            // On ouvre le menu cliqué
+            menu.style.display = 'grid';
+        }
+    });
+});
